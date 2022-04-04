43de654fc96513ea085e7739b09c4db4d27c3ac9
Gmoreno100 <gmoreno@ithreexglobal.com>
2022-04-04 17:04:13 -0300

    feat(add product)

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ import log4js from 'log4js';
 const consoleLogger = log4js.getLogger('consoleLogger');
 
 app.listen(process.env.PORT || 8000, () =>
-  consoleLogger.info(`Servidor express escuchando en el puerto ${process.env.PORT} - PID WORKER ${process.pid}`)
+  consoleLogger.info(`Servidor escuchando en el puerto ${process.env.PORT} - PID WORKER ${process.pid}`)
 );
