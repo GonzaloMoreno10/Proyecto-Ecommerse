a2150519f90aafb3aadaf7a344ff2ee1bb6e6ecb
Gmoreno100 <gmoreno@ithreexglobal.com>
2022-04-05 13:32:46 -0300

    feat(add product)

--- src/controllers/productType.ts
+++ src/controllers/productType.ts
@@ -5,26 +5,6 @@ class ProductTypeController {
     try {
       const { name } = req.params;
       const result = await productTypeRepository.getProductTypesByName(name);
-      // let productTypes = {};
-      // result.forEach((productType: any) => {
-      //   const productId = productType.categoryName;
-      //   if (!productTypes[productId]) productTypes[productId] = [];
-      //   productTypes[productId].push(productType);
-      // });
-
-      // const finalArray = [];
-      // for (const i in productTypes) {
-      //   const productTypesToReturn = [];
-      //   const finalObject: any = {};
-      //   for (const j in productTypes[i]) {
-      //     console.log(productTypes[i]);
-      //     const productType = { id: productTypes[i][j].productTypeId, name: productTypes[i][j].productTypeName };
-      //     productTypesToReturn.push(productType);
-      //     finalObject.categoryId = productTypes[i][0].categoryId;
-      //     finalObject.categoryName = productTypes[i][0].categoryName;
-      //   }
-      //   finalObject.productTypes = productTypesToReturn;
-      //   finalArray.push(finalObject);
 
       res.status(200).json(result);
     } catch (err) {
