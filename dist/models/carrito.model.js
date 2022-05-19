477992b3002b59c9983d6aa52073a9e0400c4ad3
Gmoreno100 <gmoreno@ithreexglobal.com>
2022-05-19 19:11:01 -0300

    venv

--- dist/models/carrito.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const carritoSchema = new mongoose_1.Schema({
-    userId: Object,
-    timestamp: Date,
-    productos: [Object],
-});
-exports.default = (0, mongoose_1.model)('carritos', carritoSchema);
